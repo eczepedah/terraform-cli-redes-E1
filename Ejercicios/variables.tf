eec45a9c6f1f4bc21fc44185e5bfc6aa7e0c9ced
labautistap@liverpool.com.mx <labautistap@liverpool.com.mx>
2026-09-07 09:24:25 -0600

    Add files

---
+++ Ejercicios/variables.tf
@@ -0,0 +1,20 @@
+variable "project_id" {
+  description = "ID GCP Project"
+  type        = string
+}
+
+variable "region" {
+  description = "GCP Region"
+  type        = string
+  default     = "us_central"
+}
+
+variable "bucket_name" {
+  description = "bucket"
+  type        = string
+}
+
+variable "location" {
+  description = "ubicación del bucket"
+  type        = string
+}
