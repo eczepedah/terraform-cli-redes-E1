008b35367cad023efff7c53f3c19d7148c66f6af
dlamarillasm <dlamarillas@liverpool.com.mx>
2026-09-07 09:01:53 -0600

    Primer ejercicio Terraform

---
+++ Ejercicios/variables.tf
@@ -0,0 +1,11 @@
+variable "project_id" {
+  description = "ID GCP Project"
+  type        = string
+}
+
+variable "region" {
+  description = "GCP Region"
+  type        = string
+  default     = "us_central"
+}
+
--- Ejercicios/versions.tf
+++ Ejercicios/versions.tf
@@ -1,7 +1,10 @@
 terraform {
   required_version = ">= 1.5.0"
 
-required_providers {
+  required_providers {
     google = {
       source  = "hashicorp/google"
-      version = "-> 6.0"
+      version = "~> 6.0"
+    }
+  }
+}
