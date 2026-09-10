4f6ac3df498ceb49541cd4be4ea2b0823fada2fd
Luis Rico Villarreal <lricov@liverpool.com.mx>
2026-09-10 09:19:19 -0600

    Update variables.tf

--- Ejercicios/variables.tf
+++ Ejercicios/variables.tf
@@ -1,6 +1,6 @@
 variable "project_id" {
   description = "ID GCP Project"
-  type       = string
+  type        = string
 }
 
 variable "region" {
@@ -10,15 +10,11 @@ variable "region" {
 }
 
 variable "bucket_name" {
- description = "bucket"
-  type = string
+  description = "bucket"
+  type        = string
 }
 
 variable "location" {
   description = "ubicación del bucket"
-  type = string
+  type        = string
 }
-resource "google_storage_bucket" "bucket" {
-  name          = var.bucket_name
-  location      = var.location
-  force_destroy = true
