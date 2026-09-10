2367d2156b66ed58ad9ec51a50087fef0e0cccf9
Luis Rico Villarreal <lricov@liverpool.com.mx>
2026-09-10 17:37:00 -0600

    Update versions.tf

--- Ejercicios/versions.tf
+++ Ejercicios/versions.tf
@@ -1,10 +1,10 @@
-
 terraform {
-required_version = ">= 1.5.0"
-required_providers {
-nombre_provider = {
-source = "hashicorp/google"
-version = "~> 6.0"
-}
-}
+  required_version = ">= 1.5.0"
+
+  required_providers {
+    google = {
+      source  = "hashicorp/google"
+      version = "~> 6.0"
+    }
+  }
 }
