319b0030b010cd3a892b7d7fd5ed59fa392ed2bb
dlamarillasm <dlamarillas@liverpool.com.mx>
2026-09-10 20:55:54 -0600

    Corrige configuracion Terraform

--- Ejercicios/variables.tf
+++ Ejercicios/variables.tf
@@ -6,6 +6,23 @@ variable "project_id" {
 variable "region" {
   description = "GCP Region"
   type        = string
-  default     = "us_central"
+  default     = "us-central1"
 }
 
+variable "zone" {
+  description = "GCP Zone"
+  type        = string
+  default     = "us-central1-a"
+}
+
+
+variable "bucket_name" {
+  description = "Bucket name"
+  type        = string
+}
+
+variable "location" {
+  description = "Bucket location"
+  type        = string
+  default     = "US"
+}
